6b2b395e888151f5a011d803bde47025e8533340
GanWeihao <18290605616@163.com>
2024-09-17 21:42:27 +0800

    BUG修复

--- src/Main.py
+++ src/Main.py
@@ -516,41 +516,46 @@ if __name__ == '__main__':
         is_success = False
         submit_num = 0
         while not is_success:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-                # 获取验证码
-                future_code = executor.submit(task.create_captcha)
+            try:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+                    # 获取验证码
+                    future_code = executor.submit(task.create_captcha)
 
-                # Step1: 新建草稿订单
-                future_one = executor.submit(task.create_draft, regNumber)
+                    # Step1: 新建草稿订单
+                    future_one = executor.submit(task.create_draft, regNumber)
 
-                if type == 1:
-                    # Step2: 扫描当天任意时段余票
-                    future_two = executor.submit(task.timeslot_check, begin_date, end_date, None)
-                else:
-                    # Step2: 扫描当天指定时段余票
-                    future_two = executor.submit(task.timeslot_check, begin_date, end_date, time_str)
+                    if type == 1:
+                        # Step2: 扫描当天任意时段余票
+                        future_two = executor.submit(task.timeslot_check, begin_date, end_date, None)
+                    else:
+                        # Step2: 扫描当天指定时段余票
+                        future_two = executor.submit(task.timeslot_check, begin_date, end_date, time_str)
 
-                # 等待所以线程结束
-                executor.shutdown(wait=True)
+                    # 等待所以线程结束
+                    executor.shutdown(wait=True)
 
-                # 获取线程返回的结果
-                reservationRequest_id = future_one.result()
-                arrival = future_two.result()
+                    # 获取线程返回的结果
+                    reservationRequest_id = future_one.result()
+                    arrival = future_two.result()
 
-            # Step3: 提交订单
-            submit_num += 1
-            logger.info("------准备第%d次提交订单------" % submit_num)
-            submit_result = task.submit_draft_url(arrival, reservationRequest_id)
-            if submit_result:
-                is_success = True
-            else:
+                # Step3: 提交订单
                 submit_num += 1
-                logger.info("------订单提交失败，准备第%d次尝试------" % submit_num)
-                task.create_captcha()
-                # 上述暂无余票，重新检查余票情况
-                arrival_new = task.available_slots(arrival)
-                if arrival_new is not None:
-                    is_success = task.submit_draft_url(arrival_new, reservationRequest_id)
+                logger.info("------准备第%d次提交订单------" % submit_num)
+                submit_result = task.submit_draft_url(arrival, reservationRequest_id)
+                if submit_result:
+                    is_success = True
+                else:
+                    submit_num += 1
+                    logger.info("------订单提交失败，准备第%d次尝试------" % submit_num)
+                    task.create_captcha()
+                    # 上述暂无余票，重新检查余票情况
+                    arrival_new = task.available_slots(arrival)
+                    if arrival_new is not None:
+                        is_success = task.submit_draft_url(arrival_new, reservationRequest_id)
+            except Exception as e:
+                # 处理其他所有异常
+                logger.exception("******发现错误%s，准备重试******" % e)
+
     else:
         """改签订单任务"""
         reservation_id = cfg.get("reschedule_info", "reservation_id").strip()
@@ -563,36 +568,39 @@ if __name__ == '__main__':
         task.ckeck_cookie()
         is_success = False
         reschedule_num = 0
+        while not is_success:
+            try:
+                with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+                    # Step1: 获取验证码
+                    future_one = executor.submit(task.create_captcha)
 
-        if not is_success:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-                # Step1: 获取验证码
-                future_one = executor.submit(task.create_captcha)
+                    if reschedule_type == 1:
+                        # Step2: 扫描当天任意时段余票
+                        future_two = executor.submit(task.timeslot_check, begin_date, end_date, None)
+                    else:
+                        # Step2: 扫描当天指定时段余票
+                        future_two = executor.submit(task.timeslot_check, begin_date, end_date, time_str)
 
-                if reschedule_type == 1:
-                    # Step2: 扫描当天任意时段余票
-                    future_two = executor.submit(task.timeslot_check, begin_date, end_date, None)
-                else:
-                    # Step2: 扫描当天指定时段余票
-                    future_two = executor.submit(task.timeslot_check, begin_date, end_date, time_str)
+                    # 等待所有线程结束
+                    executor.shutdown(wait=True)
 
-                # 等待所有线程结束
-                executor.shutdown(wait=True)
+                    # 获取线程返回的结果
+                    arrival = future_two.result()
 
-                # 获取线程返回的结果
-                arrival = future_two.result()
-
-            reschedule_num += 1
-            logger.info("------准备第%d次改签订单------" % reschedule_num)
-            # Step3: 改签订单
-            reschedule_result = task.reschedule(arrival, reservation_id)
-            if reschedule_result:
-                is_success = True
-            else:
                 reschedule_num += 1
-                logger.info("------改签订单失败，准备第%d次尝试------" % reschedule_num)
-                task.create_captcha()
-                # 上述暂无余票，重新检查余票情况
-                arrival_new = task.available_slots(arrival)
-                if arrival_new is not None:
-                    is_success = task.reschedule(arrival_new, reservation_id)
+                logger.info("------准备第%d次改签订单------" % reschedule_num)
+                # Step3: 改签订单
+                reschedule_result = task.reschedule(arrival, reservation_id)
+                if reschedule_result:
+                    is_success = True
+                else:
+                    reschedule_num += 1
+                    logger.info("------改签订单失败，准备第%d次尝试------" % reschedule_num)
+                    task.create_captcha()
+                    # 上述暂无余票，重新检查余票情况
+                    arrival_new = task.available_slots(arrival)
+                    if arrival_new is not None:
+                        is_success = task.reschedule(arrival_new, reservation_id)
+            except Exception as e:
+                # 处理其他所有异常
+                logger.exception("******发现错误%s，准备重试******" % e)
